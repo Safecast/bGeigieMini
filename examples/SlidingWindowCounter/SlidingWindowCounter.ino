56d9ffd069874cd384e4203351324464c4a43122
FakuFaku <fakufaku@gmail.com>
2011-12-17 19:11:09 +0900

    Added and tested and example for HardwareCounter library.

---
+++ examples/SlidingWindowCounter/SlidingWindowCounter.ino
@@ -0,0 +1,62 @@
+/*
+   HardwareCounter.ino
+   Example of using the Hardware Counter library
+
+   This example will compute the one minute number of counts using a 12 bins
+   sliding window, updating the value every 5 seconds.  Obviously, one has to
+   wait one full minute before having a correct value for the last minute.
+
+   Created 2011, Robin Scheibler aka FakuFaku
+   This example is in the public domain.
+*/
+
+#include <HardwareCounter.h>
+
+#define DELAY 5000
+#define NBINS 12
+
+static HardwareCounter counter(DELAY);  // The hardware counter object
+static unsigned long bins[NBINS] = {0}; // the sliding window
+static int index = 0;                   // the current bin index
+
+void setup()
+{
+
+  //
+  pinMode(5, INPUT);
+
+  // start serial
+  Serial.begin(9600);
+
+  // output some nice message
+  Serial.println("Start counting counts!");
+
+  // Start the counter
+  counter.start();
+
+}
+
+void loop()
+{
+  // if counter result available
+  if (counter.available())
+  {
+    int cpb = counter.count();    // fetch value of counter
+    int i;
+    int cpm = 0;
+    bins[index] = cpb;
+    for (i = 0 ; i < NBINS ; i++) // compute sum of all bins
+      cpm += bins[i];
+    index = (index+1)%NBINS;      // update bin index
+
+    // print out result
+    Serial.print("Counts in last 5 seconds: ");
+    Serial.print(cpb);
+    Serial.print(" and last minute: ");
+    Serial.println(cpm);
+
+    // restart the counter
+    counter.start();
+  }
+}
+
