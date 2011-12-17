532c234a37c70c5a11a96e3920fd432c14a614cb
FakuFaku <fakufaku@gmail.com>
2011-12-17 14:27:24 +0900

    Changed bGeigieMini example to use GPS and InterruptCounter library.

---
+++ examples/bGeigieMini/bGeigieMini.h
@@ -0,0 +1,6 @@
+#ifndef GPS_GEIGER_H
+#define GPS_GEIGER_H
+
+
+#endif
+
--- examples/bGeigieMini/bGeigieMini.ino
+++ examples/bGeigieMini/bGeigieMini.ino
@@ -2,7 +2,8 @@
 #include <chibi.h>
 #include <limits.h>
 #include <EEPROM.h>
-#include "gps_geiger.h"
+#include <GPS.h>
+#include <InterruptCounter.h>
 
 #define TIME_INTERVAL 5000
 #define NX 12
@@ -20,16 +21,6 @@ static const int chipSelect = 10;
 static const int radioSelect = A3;
 static const int sdPwr = 4;
 
-#if LED_ENABLED
-static const int greenLED = 8;
-static const int redLED = 9;
-#endif
-
-static char line[LINE_SZ];
-static byte i = 0;
-static gps_t gps;
-static unsigned long start_time;
-
 unsigned long shift_reg[NX] = {0};
 unsigned long reg_index = 0;
 unsigned long total_count = 0;
@@ -38,6 +29,12 @@ char geiger_status = VOID;
 
 // This is the data file object that we'll use to access the data file
 File dataFile; 
+
+// GPS object
+GPS *gps;
+
+// the line buffer for serial receive and send
+static char line[LINE_SZ];
 
 static char msg1[] PROGMEM = "SD init...\n";
 static char msg2[] PROGMEM = "Card failure...\n";
@@ -64,7 +61,6 @@ int log_created = 0;
 void setup()
 {
   char tmp[25];
-  gps_init();
   
   // init serial
   Serial.begin(9600);
@@ -80,9 +76,11 @@ void setup()
   pinMode(sdPwr, OUTPUT);
   digitalWrite(sdPwr, LOW);           // turn on SD card power
   
-  // attach interrupt to INT1
-  attachInterrupt(1, count_pulse, RISING);
+  // Create pulse counter on INT1
+  interruptCounterSetup(1, TIME_INTERVAL);
 
+  // Create GPS object using default Serial connection
+  gps = new GPS(&Serial, line);
   
   // set device id
   pullDevId();
@@ -130,17 +128,11 @@ void setup()
   // print free RAM. we should try to maintain about 300 bytes for stack space
   Serial.println(FreeRam());
 
-  start_time = millis();
-  //digitalWrite(sdPwr, HIGH);
+  // And now Start the Pulse Counter!
+  interruptCounterReset();
 
-#if LED_ENABLED
-  // initialize LEDs
-  pinMode(redLED, OUTPUT);
-  digitalWrite(redLED, LOW);
-  pinMode(greenLED, OUTPUT);
-  digitalWrite(greenLED, HIGH);
-#endif
-
+  // Starting now!
+  Serial.println("Starting now!");
 }
 
 /**************************************************************************/
@@ -151,76 +143,31 @@ void setup()
 void loop()
 {
   char tmp[25];
-  char *tok[SYM_SZ] = {0};
-  int j = 0;
 
-  while (Serial.available())
-  {
-    if (i < LINE_SZ)
-    {
-      line[i] = Serial.read();
-      if (line[i] == '\n')
-      {
-        line[i+1] = '\0';
-        i=0;
+  // update gps
+  gps->update();
 
-        // dump the raw GPS data
-        //Serial.print(line);
-
-        // tokenize line
-        tok[j] = strtok(line, ",");
-        do
-        {
-           tok[++j] = strtok(NULL, ",");
-        } while ((j < SYM_SZ) && (tok[j] != NULL));
-
-        // reset the index variable for next run
-        j = 0;
-
-        // parse line and date/time and update gps struct
-        if (strcmp(tok[0], "$GPRMC") == 0)
-        {
-          parse_line_rmc(tok);
-          parse_datetime();
-        }
-        else if (strcmp(tok[0], "$GPGGA") == 0)
-        {
-           parse_line_gga(tok);
-        }
-        
-        // clear the flag so that we know its okay to read the data
-        gps.updating = 0;
-      }
-      else
-      {
-        // still taking in data. increment index and make sure the updating flag is set
-        gps.updating = 1;
-        i++;
-      }
-    }
-    else
-    {
-      i = 0;
-    }
-  }
-  
   // generate CPM every TIME_INTERVAL seconds
-  if (elapsed_time(start_time) > TIME_INTERVAL)
+  if (interruptCounterAvailable())
   {
-    if (!gps.updating)
+    Serial.println(99);
+    Serial.println(gps->available());
+    if (gps->available())
     {
       unsigned long cpm=0, cpb=0;
       byte line_len;
 
-      noInterrupts();                 // XXX stop interrupts here
-      cpm = cpm_gen();                // compute last minute counts
-      cpb = shift_reg[reg_index];     // get current bin count
-      reg_index = (reg_index+1) % NX; // increment register index
-      shift_reg[reg_index] = 0;       // reset new register
-      interrupts();                   // XXX Resume interrupts here
+      // obtain the count in the last bin
+      cpb = interruptCounterCount();
+      Serial.println(cpb);
 
-      // update the start time for the next interval
-      start_time = millis();
+      // reset the pulse counter
+      interruptCounterReset();
+
+      // insert count in sliding window and compute CPM
+      shift_reg[reg_index] = cpb;     // put the count in the correct bin
+      reg_index = (reg_index+1) % NX; // increment register index
+      cpm = cpm_gen();                // compute sum over all bins
 
       // update the total counter
       total_count += cpb;
@@ -243,23 +190,16 @@ void loop()
 
       line_len = gps_gen_timestamp(line, shift_reg[reg_index], cpm, cpb);
 
-#if LED_ENABLED
-      // check GPS status and blink LED accordingly
-      if (gps.status[0] == AVAILABLE && geiger_status == AVAILABLE)
-        blink(greenLED, 4, 100, 100);
-      else
-        blink(redLED, 4, 100, 100);
-#endif
 
-      if (gps_init_acq == 0 && gps.status[0] == AVAILABLE)
+      if (gps_init_acq == 0 && gps->getData()->status[0] == AVAILABLE)
       {
         // flag GPS acquired
         gps_init_acq = 1;
         // Create the filename for that drive
         strcpy(filename, dev_id);
         strcat(filename, "-");
-        strncat(filename, gps.date+2, 2);
-        strncat(filename, gps.date, 2);
+        strncat(filename, gps->getData()->date+2, 2);
+        strncat(filename, gps->getData()->date, 2);
         // print some comment line to mark beginning of new log
         strcpy(filename+8, ext_log);
         dataFile = SD.open(filename, FILE_WRITE);
@@ -366,135 +306,42 @@ void loop()
 
 */
 /**************************************************************************/
-static void gps_init()
-{
-  memset(&gps, 0, sizeof(gps_t));
-  
-  // set static values first
-  strcpy(gps.dev_name, "GT-703");
-  strcpy(gps.meas_type, "AIR");
-}
-
-/**************************************************************************/
-/*!
-
-*/
-/**************************************************************************/
-static void parse_line_rmc(char **token)
-{
-  memcpy(&gps.utc,        token[1],     UTC_SZ-1);
-  memcpy(&gps.status,     token[2],     DEFAULT_SZ-1);
-  memcpy(&gps.lat,        token[3],     LAT_SZ-1);
-  memcpy(&gps.lat_hem,    token[4],     DEFAULT_SZ-1);
-  memcpy(&gps.lon,        token[5],     LON_SZ-1);
-  memcpy(&gps.lon_hem,    token[6],     DEFAULT_SZ-1);
-  memcpy(&gps.speed,      token[7],     SPD_SZ-1);
-  memcpy(&gps.course,     token[8],     CRS_SZ-1);
-  memcpy(&gps.date,       token[9],     DATE_SZ-1);
-  memcpy(&gps.checksum,   token[10],    CKSUM_SZ-1);
-}
-
-/**************************************************************************/
-/*!
-
-*/
-/**************************************************************************/
-void parse_line_gga(char **token)
-{
-    memcpy(&gps.quality,    token[6], DEFAULT_SZ-1);
-    memcpy(&gps.num_sat,   token[7], NUM_SAT_SZ-1);
-    memcpy(&gps.precision, token[8], PRECISION_SZ-1);
-    memcpy(&gps.altitude,  token[9], ALTITUDE_SZ-1);
-}
-
-
-/**************************************************************************/
-/*!
-
-*/
-/**************************************************************************/
-void parse_datetime()
-{
-    memset(&gps.datetime, 0, sizeof(date_time_t));
-
-    // parse UTC time
-    memcpy(gps.datetime.hour, &gps.utc[0], 2);
-    memcpy(gps.datetime.minute, &gps.utc[2], 2);
-    memcpy(gps.datetime.second, &gps.utc[4], 2);
-
-    // parse UTC calendar
-    memcpy(gps.datetime.day, &gps.date[0], 2);
-    memcpy(gps.datetime.month, &gps.date[2], 2);
-    memcpy(gps.datetime.year, &gps.date[4], 2);
-}
-
-/**************************************************************************/
-/*!
-
-*/
-/**************************************************************************/
 byte gps_gen_timestamp(char *buf, unsigned long counts, unsigned long cpm, unsigned long cpb)
 {
   byte len;
   byte chk;
+
+  gps_t *ptr = gps->getData();
   
   memset(buf, 0, LINE_SZ);
   sprintf(buf, "$%s,%s,20%s-%s-%sT%s:%s:%sZ,%ld,%ld,%ld,%c,%s,%s,%s,%s,%s,%s,%s,%s",  \
               hdr, \
               dev_id, \
-              gps.datetime.year, gps.datetime.month, gps.datetime.day,  \
-              gps.datetime.hour, gps.datetime.minute, gps.datetime.second, \
+              ptr->datetime.year, ptr->datetime.month, ptr->datetime.day,  \
+              ptr->datetime.hour, ptr->datetime.minute, ptr->datetime.second, \
               cpm, \
               cpb, \
               total_count, \
               geiger_status, \
-              gps.lat, gps.lat_hem, \
-              gps.lon, gps.lon_hem, \
-              gps.altitude, \
-              gps.status, \
-              gps.precision, \
-              gps.quality);
+              ptr->lat, ptr->lat_hem, \
+              ptr->lon, ptr->lon_hem, \
+              ptr->altitude, \
+              ptr->status, \
+              ptr->precision, \
+              ptr->quality);
    len = strlen(buf);
    buf[len] = '\0';
 
    // generate checksum
-   chk = checksum(buf+1, len);
+   chk = GPS::checksum(buf+1, len);
 
    // add checksum to end of line before sending
    if (chk < 16)
      sprintf(buf + len, "*0%X", (int)chk);
    else
      sprintf(buf + len, "*%X", (int)chk);
-      
 
    return len;
-}
-
-/**************************************************************************/
-// calculate elapsed time
-/**************************************************************************/
-unsigned long elapsed_time(unsigned long start_time)
-{
-  unsigned long stop_time = millis();
-  
-  if (start_time >= stop_time)
-  {
-    return start_time - stop_time;
-  }
-  else
-  {
-    return (ULONG_MAX - (start_time - stop_time));
-  }
-}
-
-/**************************************************************************/
-/*!
-
-*/
-/**************************************************************************/
-void count_pulse()
-{
-  shift_reg[reg_index]++;
 }
 
 /**************************************************************************/
@@ -514,22 +361,6 @@ unsigned long cpm_gen()
    return c_p_m;
 }
 
-/**************************************************************************/
-/*!
-compute check sum of N bytes in array s
-*/
-/**************************************************************************/
-char checksum(char *s, int N)
-{
-  int i = 0;
-  char chk = s[0];
-
-  for (i=1 ; i < N ; i++)
-    chk ^= s[i];
-
-  return chk;
-}
-
 void pullDevId()
 {
   for (int i=0 ; i < BMRDD_ID_LEN ; i++)
@@ -543,22 +374,4 @@ void pullDevId()
   }
   dev_id[BMRDD_ID_LEN] = NULL;
 }
-
-#if LED_ENABLED
-void blink(int LED, int p, int t_up, int t_dw)
-{
-  if (p == 0)
-    return;
-
-  while (p > 0)
-  {
-    digitalWrite(LED, HIGH);
-    delay(t_up);
-    digitalWrite(LED, LOW);
-    p--;
-    if (p > 0)
-      delay(t_dw);
-  }
-}
-#endif
 
