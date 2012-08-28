5550267236e7c260fec6b371cd281c3bd9cf0156
FakuFaku <fakufaku@gmail.com>
2012-08-28 13:10:37 +0900

    Corrected bGeigie mini version to 1.3.9 in the version header.

--- examples/bGeigieMini/version.h
+++ examples/bGeigieMini/version.h
@@ -3,9 +3,9 @@
 #define __VERSION_H__
 
 #if JAPAN_POST
-static const char version[] = "1.3.7jp";
+static const char version[] = "1.3.9jp";
 #else
-static const char version[] = "1.3.7";
+static const char version[] = "1.3.9";
 #endif
 
 #endif /* __VERSION_H__ */
