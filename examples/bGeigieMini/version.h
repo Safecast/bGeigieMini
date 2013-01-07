5484af9a21d385ab59005aedc81bd9bfa6e2fa2f
FakuFaku <fakufaku@gmail.com>
2013-01-07 14:49:07 +0900

    Bump bGeigie-mini to v1.4.3.

--- examples/bGeigieMini/version.h
+++ examples/bGeigieMini/version.h
@@ -3,9 +3,9 @@
 #define __VERSION_H__
 
 #if JAPAN_POST
-static const char version[] = "1.4.2jp";
+static const char version[] = "1.4.3jp";
 #else
-static const char version[] = "1.4.2";
+static const char version[] = "1.4.3";
 #endif
 
 #endif /* __VERSION_H__ */
